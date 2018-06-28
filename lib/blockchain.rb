293b1e6c92794f62929d9ec355e2f5f9e36a5257
Isaac Anthony <ianthony@optoro.com>
2018-06-28 13:09:06 -0400

    06-28 Code Shop attempt to fill out Blockchain#block_valid?

--- lib/blockchain.rb
+++ lib/blockchain.rb
@@ -24,6 +24,21 @@ class Blockchain
   # negative balance?
   #
   def block_valid?(block)
-    # ...
+    block.valid?(2) && block.parent_digest == head.digest && verify_transactions
+  end
+
+  def verify_transactions
+    balances = Hash.new(0)
+
+    blocks.each do |block|
+      block.transactions.each do |transaction|
+        balances[transaction[:from]] -= transaction[:amount] unless transaction[:from].nil?
+        balances[transaction[:to]] += transaction[:amount]
+      end
+    end
+
+    # loop through balances and see if any value is negative, except for key of nil
+    puts balances
+    balances.values.all? { |balance| balance >= 0 }
   end
 end
