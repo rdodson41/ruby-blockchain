cd8bcf006a55c06622e9f9aee6cc9b94750f2c07
seisen77 <seisen@optoro.com>
2018-08-09 11:57:00 -0400

    Codeshop on 8/9/18 - implemented user methods

--- lib/user.rb
+++ lib/user.rb
@@ -7,10 +7,15 @@ class User
   def initialize(blockchain, name, initial_balance)
     @blockchain = blockchain
     @name = name
-    # ...
+    block = Block.new(blockchain.head_digest, [{ from: nil, to: name, amount: initial_balance }])
+    block.mine!
+    blockchain.add_block!(block)
   end
 
+  # user should only be able to send if their balance will not go negative
   def send!(user, amount)
-    # ...
+    block = Block.new(blockchain.head_digest, [{from: name, to: user.name, amount: amount}])
+    block.mine!
+    blockchain.add_block!(block)
   end
 end
